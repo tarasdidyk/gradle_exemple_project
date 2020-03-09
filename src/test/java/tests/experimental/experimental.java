82a5a7a10b91bef71b4cd915b9d6a7d454c9f0b1
dtr <tarasi44444@gmail.com>
2020-03-09 13:45:08 +0200

    Edit experimental

--- src/test/java/tests/experimental/experimental.java
+++ src/test/java/tests/experimental/experimental.java
@@ -4,16 +4,22 @@ package tests.experimental;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import utils.ScreenshotListener;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -24,7 +30,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
 import static org.testng.Assert.assertEquals;
 
-public class experimental {
+public class experimental extends TestListenerAdapter {
 
     RemoteWebDriver remoteWebDriver;
 
@@ -42,5 +48,20 @@ public class experimental {
     @Step("open google ")
     public void openGoogle() {
         remoteWebDriver.get("https://google.com");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        saveScreenshot();
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        saveScreenshot();
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot() {
+        return ((TakesScreenshot) remoteWebDriver).getScreenshotAs(OutputType.BYTES);
     }
 }
