f2451625422ad3cfc98d337da76d3575d0e6fc47
dtr <tarasi44444@gmail.com>
2020-03-09 13:33:19 +0200

    Refactor experimental

--- src/test/java/tests/experimental/experimental.java
+++ src/test/java/tests/experimental/experimental.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -23,20 +24,19 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
 import static org.testng.Assert.assertEquals;
 
-
 public class experimental {
 
     RemoteWebDriver remoteWebDriver;
 
     @Test
     void ableToRunDefaultDriverOnSelenoid() throws MalformedURLException {
-        String url = "http://192.168.0.102:4444/wd/hub";
+       /* String url = "http://192.168.0.102:4444/wd/hub";
         ChromeOptions options = new ChromeOptions();
         remoteWebDriver = new RemoteWebDriver(new URL(url), options);
-        openGoogle();
-      /*  Configuration.driverManagerEnabled = false;
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        open("https://www.google.com");*/
+        openGoogle();*/
+        Configuration.driverManagerEnabled = false;
+        Configuration.remote = "http://192.168.0.102:4444/wd/hu";
+        open("https://google.com");
     }
 
     @Step("open google ")
