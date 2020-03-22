4b1919d88af4ff23dab8883b1cc54bdc3641d7b2
dtr <tarasi44444@gmail.com>
2020-03-22 18:23:06 +0200

    Delete unused files

--- src/test/java/utils/DriverHelper.java
+++ src/test/java/utils/DriverHelper.java
@@ -8,14 +8,7 @@ import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.element;
 
 public class DriverHelper extends BaseTest {
-  private static final int TIMEOUT_SHORT = 30;
-
-  SelenideWait selenideWait;
-
-  public DriverHelper() {
-
-  }
-
+  
   public void selectMenuItem(String menuItemName) {
     element(By.xpath("//ul[contains(@class, 'dropdown-menu')]")).should(appear);
     String locator = String.format("//ul[contains(@class, 'dropdown-menu')]" +
