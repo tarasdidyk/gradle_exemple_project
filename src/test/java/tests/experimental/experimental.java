5264bdb0422841f3e81f16fa18f734b0a4cbdaad
dtr <tarasi44444@gmail.com>
2020-03-25 19:11:38 +0200

    Edit experimental

--- src/test/java/tests/experimental/experimental.java
+++ src/test/java/tests/experimental/experimental.java
@@ -2,19 +2,32 @@ package tests.experimental;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import pages.HomePage;
 import tests.common.BaseTest;
+import utils.Utils;
 import utils.enums.AboutPolytechnics;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class experimental extends BaseTest {
-  HomePage homePage;
+public class experimental {
+  /*HomePage homePage;
 
   @BeforeEach
   public void setup() {
@@ -32,5 +45,74 @@ public class experimental extends BaseTest {
   @AfterEach
   public void holdBrowserOpen() {
     Configuration.holdBrowserOpen = true;
+  }*/
+
+  RemoteWebDriver driver;
+  WebDriver driver1;
+  @FindBy(linkText = "some-file.txt")
+  private WebElement downloadFile;
+
+  public void init() {
+    if (Utils.isEnvironmentRemote()) {
+      String downloadFilepath =  System.getProperty("/var/lib/");
+      HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+      chromePrefs.put("profile.default_content_settings.popups", 0);
+      chromePrefs.put("download.default_directory", downloadFilepath);
+      ChromeOptions options = new ChromeOptions();
+      options.setExperimentalOption("prefs", chromePrefs);
+      DesiredCapabilities cap = DesiredCapabilities.chrome();
+      cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+      cap.setCapability(ChromeOptions.CAPABILITY, options);
+      URL host = null;
+      try {
+        host = new URL("http://192.168.0.105:4444/wd/hub");
+      } catch (MalformedURLException e) {
+        e.printStackTrace();
+      }
+      driver = new RemoteWebDriver(host, cap);
+      driver.setFileDetector(new LocalFileDetector());
+ /*   System.setProperty("webdriver.chrome.driver",
+        new File(this.getClass().getResource("/chromedriver.exe").getFile())
+            .getPath());
+    WebDriver driver = new ChromeDriver(cap);*/
+    } else {
+      String downloadFilepath =  System.getProperty("user.dir");
+      //String downloadFilepath = "C:\\1";
+      HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+      chromePrefs.put("profile.default_content_settings.popups", 0);
+      chromePrefs.put("download.default_directory", downloadFilepath);
+      ChromeOptions options = new ChromeOptions();
+      options.setExperimentalOption("prefs", chromePrefs);
+      DesiredCapabilities cap = DesiredCapabilities.chrome();
+      cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+      cap.setCapability(ChromeOptions.CAPABILITY, options);
+      System.setProperty("webdriver.chrome.driver",
+          new File(this.getClass().getResource("/chromedriver.exe").getFile())
+              .getPath());
+      driver1 = new ChromeDriver(cap);
+    }
+  }
+
+  @Test
+  public void downloadLogLevelReportNetNetworkTest() throws InterruptedException {
+    init();
+    if (Utils.isEnvironmentRemote()) {
+      PageFactory.initElements(driver, this);
+      driver.get("http://the-internet.herokuapp.com/download");
+    } else {
+      PageFactory.initElements(driver1, this);
+      driver1.get("http://the-internet.herokuapp.com/download");
+    }
+    downloadFile.click();
+    Thread.sleep(60000);
+  }
+
+  @AfterAll
+  public void quiet(){
+    if (Utils.isEnvironmentRemote()) {
+      driver.quit();
+    } else {
+      driver1.quit();
+    }
   }
 }
